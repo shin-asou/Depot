610cd07d4882c4ad6675fcb0b82f0d684c982d64
shin-asou <shin.asou0120@gmail.com>
2013-01-19 22:04:47 +0900

    cartのマイグレーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130119123522) do
+ActiveRecord::Schema.define(:version => 20130119125157) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
